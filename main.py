65d0851ad50765d2aab82f31866b2c849632cf7e
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-26 02:42:15 +0900

    Update and rename index.py to main.py

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='>>')
 
+bot.run('my-bot-token')
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
 async def hello(ctx):
     await ctx.send('hello!')
 
-
-bot.run('my-bot-token')
+@bot.command()
+async def owo(ctx, args):
+    await ctx.send(args)

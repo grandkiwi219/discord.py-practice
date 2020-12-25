190ecbc5e5fc04dc02f6be6f58faba4e8b788d4b
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-26 02:34:51 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
 import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>>')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+async def hello(ctx):
+    await ctx.send('hello!')
+
+
+bot.run('my-bot-token')

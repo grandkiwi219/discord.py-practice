c9755f10f4ce3e671a69b487bbfbb1debee28828
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-26 02:55:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='>>')
+bot = commands.Bot(command_prefix='>>', owner_id='385725000432484355')
 
 bot.run('my-bot-token')
 
